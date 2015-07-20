e1d2a0badc3df48cbc777ba3719165f373bf59f0
baswerc <corey.baswell@gmail.com>
2015-07-19 19:40:42 -0500

    Working on NIO SSL

--- src/main/java/org/baswell/httproxy/SSLEngineBuffer.java
+++ src/main/java/org/baswell/httproxy/SSLEngineBuffer.java
@@ -47,264 +47,203 @@ public class SSLEngineBuffer
     emptyBuffer = ByteBuffer.allocate(0);
   }
 
-  synchronized int unwrap(SocketChannel socketChannel, ByteBuffer inputBuffer, boolean wrapIfNecessary) throws IOException
+  synchronized int unwrap(SocketChannel socketChannel, ByteBuffer applicationInputBuffer, boolean wrapIfNecessary) throws IOException
   {
     if (logDebug) log.info("unwrap:");
 
     int totalReadFromChannel = 0;
 
-    try
+    // Keep looping until peer has no more data ready or the applicationInboundBuffer is full
+    WRAP: do
     {
-      // Keep looping until peer has no more data ready or the applicationInboundBuffer is full
-      WRAP: while (true)
+      // 1. Pull data from peer into networkInboundBuffer
+
+      int readFromChannel = 0;
+      while (networkInboundBuffer.hasRemaining())
       {
-        // 1. Pull data from peer into networkInboundBuffer
-
-        int readFromChannel = 0;
-        while (networkInboundBuffer.hasRemaining())
+        int read = socketChannel.read(networkInboundBuffer);
+        if (logDebug) log.info("unwrap: socket read " + read + "(" + readFromChannel + ", " + totalReadFromChannel + ")");
+        if (read <= 0)
         {
-          int read = socketChannel.read(networkInboundBuffer);
-          if (logDebug) log.info("unwrap: socket read " + read + "(" + readFromChannel + ", " + totalReadFromChannel + ")");
-          if (read <= 0)
+          if ((read < 0) && (readFromChannel == 0) && (totalReadFromChannel == 0))
           {
-            if ((read < 0) && (readFromChannel == 0) && (totalReadFromChannel == 0))
-            {
-              // No work done and we've reached the end of the channel from peer
-              if (logDebug) log.info("unwrap: exit: end of channel");
-              return read;
-            }
-            break;
+            // No work done and we've reached the end of the channel from peer
+            if (logDebug) log.info("unwrap: exit: end of channel");
+            return read;
           }
-          else
-          {
-            readFromChannel += read;
-          }
+          break;
         }
-
-        networkInboundBuffer.flip();
-        totalReadFromChannel += readFromChannel;
-
-        try
+        else
         {
-          SSLEngineResult result = sslEngine.unwrap(networkInboundBuffer, inputBuffer);
-          if (logDebug) log.info("unwrap: result: " + result);
-
-          switch (result.getStatus())
-          {
-            case OK:
-              SSLEngineResult.HandshakeStatus handshakeStatus = result.getHandshakeStatus();
-              switch (handshakeStatus)
-              {
-                case NEED_UNWRAP:
-                  break;
-
-                case NEED_WRAP:
-                  if (wrapIfNecessary)
-                  {
-                    wrap(socketChannel, emptyBuffer, false);
-                  }
-                  else
-                  {
-                    break WRAP;
-                  }
-                  break;
-
-                case NEED_TASK:
-                  runHandshakeTasks();
-                  break;
-
-                case NOT_HANDSHAKING:
-                default:
-                  break;
-              }
-              break;
-
-            case BUFFER_OVERFLOW:
-              break WRAP;
-
-            case CLOSED:
-              if (logDebug) log.info("unwrap: exit: ssl closed");
-              return totalReadFromChannel == 0 ? -1 : totalReadFromChannel;
-
-            case BUFFER_UNDERFLOW:
-              break WRAP;
-          }
-        }
-        finally
-        {
-          networkInboundBuffer.compact();
+          readFromChannel += read;
         }
       }
+
+      networkInboundBuffer.flip();
+      totalReadFromChannel += readFromChannel;
+
+      try
+      {
+        SSLEngineResult result = sslEngine.unwrap(networkInboundBuffer, applicationInputBuffer);
+        if (logDebug) log.info("unwrap: result: " + result);
+
+        switch (result.getStatus())
+        {
+          case OK:
+            SSLEngineResult.HandshakeStatus handshakeStatus = result.getHandshakeStatus();
+            switch (handshakeStatus)
+            {
+              case NEED_UNWRAP:
+                break;
+
+              case NEED_WRAP:
+                if (wrapIfNecessary)
+                {
+                  wrap(socketChannel, emptyBuffer, false);
+                }
+                else
+                {
+                  break WRAP;
+                }
+                break;
+
+              case NEED_TASK:
+                runHandshakeTasks();
+                break;
+
+              case NOT_HANDSHAKING:
+              default:
+                break;
+            }
+            break;
+
+          case BUFFER_OVERFLOW:
+            if (logDebug) log.debug("unwrap: buffer overflow");
+            break WRAP;
+
+          case CLOSED:
+            if (logDebug) log.info("unwrap: exit: ssl closed");
+            return totalReadFromChannel == 0 ? -1 : totalReadFromChannel;
+
+          case BUFFER_UNDERFLOW:
+            if (logDebug) log.debug("unwrap: buffer underflow");
+            break WRAP;
+        }
+      }
+      finally
+      {
+        networkInboundBuffer.compact();
+      }
     }
-    finally
-    {
-      System.out.println("unwrap: application staging: " + inputBuffer.position());
-//      applicationInboundBuffer.flip();
-    }
+    while (applicationInputBuffer.hasRemaining());
+
+    return totalReadFromChannel;
   }
 
-  /*
-   * Buffer pre and post conditions:
-   *
-   * networkOutboundBuffer pre: read, post read:
-   * applicationOutboundBuffer pre: write, post: write
-   *
-   */
-  synchronized int wrap(SocketChannel socketChannel, ByteBuffer applicationOutboundBuffer) throws IOException
+  synchronized int wrap(SocketChannel socketChannel, ByteBuffer applicationOutboundBuffer, boolean unwrapIfNecessary) throws IOException
   {
     if (logDebug) log.info("wrap");
     int totalWritten = 0;
 
-    // sslEngine.wrap(applicationOutboundBuffer, networkOutboundBuffer);
-
-    // 1. Any data already wrapped ? Go ahead and send that.
-    while (networkOutboundBuffer.hasRemaining())
-    {
-      int written = socketChannel.write(networkOutboundBuffer);
-      totalWritten += written;
-      if (logDebug) log.info("wrap: pre socket write: " + written + " (" + totalWritten + ")");
-
-      if (written <= 0)
-      {
-        return (totalWritten == 0 && written < 0) ? written : totalWritten;
-      }
-    }
+    // 1. Send any data already wrapped out channel
 
     if (networkOutboundBuffer.hasRemaining())
     {
-      return totalWritten;
+      totalWritten = send(socketChannel, networkOutboundBuffer);
+      if (totalWritten < 0)
+      {
+        return totalWritten;
+      }
     }
 
     // 2. Any data in application buffer ? Wrap that and send it to peer.
 
-    networkOutboundBuffer.compact();
-    try
+    WRAP: do
     {
-      WRAP: while (applicationOutboundBuffer.hasRemaining() || networkOutboundBuffer.hasRemaining())
+      networkOutboundBuffer.compact();
+      SSLEngineResult result = sslEngine.wrap(applicationOutboundBuffer, networkOutboundBuffer);
+      if (logDebug) log.info("wrap: result: " + result);
+
+      networkOutboundBuffer.flip();
+      if (networkOutboundBuffer.hasRemaining())
       {
-        System.out.println("wrap: application outbound: " + applicationOutboundBuffer.remaining());
-        SSLEngineResult result = sslEngine.wrap(applicationOutboundBuffer, networkOutboundBuffer);
-        System.out.println("wrap: application outbound: " + applicationOutboundBuffer.remaining());
-        if (logDebug) log.info("wrap: result: " + result);
-        networkOutboundBuffer.flip();
-        try
+        int written = send(socketChannel, networkOutboundBuffer);
+        if (written < 0)
         {
-          // Was any encrypted application data produced ? If so go ahead and try to send to peer.
-          int written = 0;
-          while (networkOutboundBuffer.hasRemaining())
-          {
-            int nextWritten = socketChannel.write(networkOutboundBuffer);
-
-            if (nextWritten == 0)
-            {
-              break;
-            }
-            else if (nextWritten < 0)
-            {
-              totalWritten += written;
-              return (totalWritten == 0) ? nextWritten : totalWritten;
-            }
-            written += nextWritten;
-          }
-
-          if (logDebug) log.info("wrap: socket write: " + written + " (" + totalWritten + ")");
-
-          totalWritten += written;
-
-          switch (result.getStatus())
-          {
-            case OK:
-              SSLEngineResult.HandshakeStatus handshakeStatus = result.getHandshakeStatus();
-              switch (handshakeStatus)
-              {
-                case NEED_WRAP:
-                  // Not enough data in applicationOutboundBuffer.
-                  if (written == 0)
-                  {
-                    if (logDebug) log.info("wrap: exit: need wrap & no data written");
-                    break WRAP;
-                  }
-                  break;
-
-                case NEED_UNWRAP:
-                  applicationStagingInboundBuffer.clear();
-                  if (unwrap(false, applicationStagingInboundBuffer) == 0)
-                  {
-                    break WRAP;
-                  }
-                  break;
-                  /*
-                  if (unwrap(false) == 0)
-                  {
-                    // Don't hold selector thread up waiting on data from peer
-                    if (logDebug) log.info("wrap: exit: unwrap and no data read");
-                    break WRAP;
-                  }
-                  */
-                //break UNWRAP;
-                  /*
-                  if (unwrapIfNecessary)
-                  {
-                    if (unwrap(false) == 0)
-                    {
-                      // Don't hold selector thread up waiting on data from peer
-                      if (logDebug) log.info("wrap: exit: unwrap and no data read");
-                      break UNWRAP;
-                    }
-                    else
-                    {
-                      break;
-                    }
-                  }
-                  else
-                  {
-                    if (logDebug) log.info("wrap: exit: unwrap and unwrap not allowed");
-                    break UNWRAP;
-                  }
-                  */
-
-                case NEED_TASK:
-                  runHandshakeTasks();
-                  if (logDebug) log.info("wrap: exit: need tasks");
-                  break;
-
-                case NOT_HANDSHAKING:
-                  if (written <= 0)
-                  {
-                    if (logDebug) log.info("wrap: exit: no data written");
-                    break WRAP;
-                  }
-              }
-              break;
-
-            case BUFFER_OVERFLOW:
-              throw new IOException("Buffer overflow.");
-              //System.out.println(applicationOutboundBuffer.limit() + " " + applicationOutboundBuffer.remaining() + " / " + networkOutboundBuffer.limit() + " " + networkOutboundBuffer.remaining());
-              //break WRAP;
-
-            case CLOSED:
-              if (logDebug) log.info("wrap: exit: closed");
-              break WRAP;
-
-            case BUFFER_UNDERFLOW:
-              // Need more data in applicationOutboundBuffer
-              if (logDebug) log.info("wrap: exit: buffer underflow");
-              break WRAP;
-          }
+          return totalWritten == 0 ? written : totalWritten;
         }
-        finally
+        else
         {
-          networkOutboundBuffer.compact();
+          totalWritten += written;
         }
       }
+
+      switch (result.getStatus())
+      {
+        case OK:
+          SSLEngineResult.HandshakeStatus handshakeStatus = result.getHandshakeStatus();
+          switch (handshakeStatus)
+          {
+            case NEED_WRAP:
+              break;
+
+            case NEED_UNWRAP:
+              if (unwrapIfNecessary)
+              {
+                unwrap(socketChannel, emptyBuffer, false);
+                break;
+              }
+              else
+              {
+                break WRAP;
+              }
+
+            case NEED_TASK:
+              runHandshakeTasks();
+              if (logDebug) log.info("wrap: exit: need tasks");
+              break;
+
+            case NOT_HANDSHAKING:
+              break;
+          }
+          break;
+
+        case BUFFER_OVERFLOW:
+          if (logDebug) log.info("wrap: exit: buffer overflow");
+          break WRAP;
+
+        case CLOSED:
+          if (logDebug) log.info("wrap: exit: closed");
+          break WRAP;
+
+        case BUFFER_UNDERFLOW:
+          if (logDebug) log.info("wrap: exit: buffer underflow");
+          break WRAP;
+      }
     }
-    finally
-    {
-      networkOutboundBuffer.flip();
-    }
+    while (applicationOutboundBuffer.hasRemaining());
 
     if (logDebug) log.info("wrap: return: " + totalWritten);
+    return totalWritten;
+  }
 
+  int send(SocketChannel channel, ByteBuffer buffer) throws IOException
+  {
+    int totalWritten = 0;
+    while (buffer.hasRemaining())
+    {
+      int written = channel.write(buffer);
+
+      if (written == 0)
+      {
+        break;
+      }
+      else if (written < 0)
+      {
+        return (totalWritten == 0) ? written : totalWritten;
+      }
+      totalWritten += written;
+    }
     return totalWritten;
   }
 
